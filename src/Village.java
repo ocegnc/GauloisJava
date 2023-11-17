9e04aac8b5c8d93ab871762b3d64398b410c0591
AubergeM <malaury-auberge@etud.univ-jfc.fr>
2023-11-17 12:29:41 +0100

    v5 village

--- src/Village.java
+++ src/Village.java
@@ -23,7 +23,49 @@ public class Village extends Groupe {
         }
     }
 
+    private boolean estTerminee(Camp c){
+        boolean v = true;
+        for (Humains r : c.lesHabitants){
+            if (r.force!=0){//au moins 1 romain a de la force donc bataille pas termine
+                v = false;
+            }
+        }
+        return v;
+    }
+
     public String bataille1(Camp c){
+        String mes = "";
+        String result = "";
+        while (estTerminee(c)==false) {
+            for (Humains r : c.lesHabitants) {
+                for (Humains g : lesHabitants) {
+                    if (g.metier != "chef" && g.metier && "druide") {
+                        mes = "Le romain se prend une baffe";
+                        r.force -= (1 / 6) * g.force;
+                        g.force -= r.force;
+                    }
+                }
+            }
+        }
+        result = "La bataille est terminée";
+        return mes + result;
+    }
+
+    public String bataille2(Camp c){
+        for(Humains g : lesHabitants){
+            if (g.force < 5){
+
+            }
+            if (g.force>=5 && g.force<10){
+
+            }
+            if (g.force >= 10 && g.force < 15) {
+
+            }
+            if (g.force>=15){
+
+            }
+        }
 
     }
 }
