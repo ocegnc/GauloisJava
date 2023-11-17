d231b744237a21db5177b3aa290c82b9340f096a
ocegnc <oceane.guennec@gmail.com>
2023-11-17 12:32:12 +0100

    commit oc dernier

--- src/Gaulois.java
+++ src/Gaulois.java
@@ -1,23 +1,26 @@
+import java.util.Random;
 
 public class Gaulois extends Humains{
     private String metier;
 
-    public Gaulois(String nom, int force, String metier){
-        super(nom, force);
+    public Gaulois(String nom, String metier){
+        super(nom, 1);
         this.metier=metier;
+    }
+    public Gaulois(){
+        super("Obélix",15);
+        this.metier="Tailleur de menhir";
     }
 
     public String toString(){
         return "Gaulois : "+super.toString()+" , métier : "+metier;
     }
 
-    public String getNom(){
-        return nom;
-    }
-
-    public String rencontre(Humains g){
-        g=(Gaulois) g;
-        return "Bonjour je suis"+g.getNom()+" /n Bonjour "+g.getNom()+"moi c'est "+this.getNom();
+    public String rencontre(){
+        Random r=new Random();
+        int i = r.nextInt(lesHabitants.size());
+        Humains g = lesHabitants.get(i);
+        return "Bonjour je suis "+nom+"\n Bonjour "+nom+" moi c'est "+g.getNom();
     }
 }
 
